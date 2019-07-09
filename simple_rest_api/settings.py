6f2b69a00fc71f73d83524ac0cc53cc6e3d2179a
M.Tareykin <m.tareykin@pr-solution.ru>
2019-07-09 15:39:34 +0300

    add static file settings

--- simple_rest_api/settings.py
+++ simple_rest_api/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/content/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'content')
